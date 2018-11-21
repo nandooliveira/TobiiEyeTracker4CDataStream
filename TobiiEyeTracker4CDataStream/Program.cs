cbe9aab3cf6bc1b0ed6759aef106f85da5938008
Fernando Oliveira <nandooliveira.al@gmail.com>
2018-11-21 01:17:43 -0300

    first version

--- TobiiEyeTracker4CDataStream/Program.cs
+++ TobiiEyeTracker4CDataStream/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.IO;
+using Tobii.Interaction;
 
 namespace TobiiEyeTracker4CDataStream
 {
     class Program
     {
+        private const string Format = "{0}\t{1}\t{2}";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = new Host();
+
+            var gazePointDataStream = host.Streams.CreateGazePointDataStream();
+            Console.WriteLine("testanco");
+
+            // Open file to write data
+            FileStream fs = new FileStream(@"C:\Users\nando\Desktop\gaze_data.txt", FileMode.Append);
+            StreamWriter sw = new StreamWriter(fs);
+
+            // 3. Get the gaze data!
+            gazePointDataStream.GazePoint((x, y, ts) => {
+                string now = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+                string point = string.Format(Format, (int)x, (int)y, now);
+                sw.WriteLine(point);
+                Console.WriteLine(point);
+            });
+
+            // okay, it is 4 lines, but you won't be able to see much without this one :)
+            Console.ReadKey();
+
+            // we will close the coonection to the Tobii Engine before exit.
+            host.DisableConnection();
+
+            sw.Flush();
+            sw.Close();
+            fs.Close();
         }
     }
 }
