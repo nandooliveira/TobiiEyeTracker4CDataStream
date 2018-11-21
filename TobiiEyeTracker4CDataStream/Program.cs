9a574e6ee233e87605a3386eea399e74d5ffef25
Fernando Oliveira <nandooliveira.al@gmail.com>
2018-11-21 02:10:03 -0300

    save data on postgres instead of file

--- TobiiEyeTracker4CDataStream/Program.cs
+++ TobiiEyeTracker4CDataStream/Program.cs
@@ -1,41 +1,61 @@
 ﻿using System;
 using System.IO;
 using Tobii.Interaction;
+using Npgsql;
+using System.Data;
 
 namespace TobiiEyeTracker4CDataStream
 {
     class Program
     {
+        private DataSet ds = new DataSet();
+        private DataTable dt = new DataTable();
+
         private const string Format = "{0}\t{1}\t{2}";
 
         static void Main(string[] args)
         {
+            var connString = "Host=localhost;Username=postgres;Password=postgres;Database=eyetracker";
             var host = new Host();
-
             var gazePointDataStream = host.Streams.CreateGazePointDataStream();
-            Console.WriteLine("testanco");
 
             // Open file to write data
-            FileStream fs = new FileStream(@"C:\Users\nando\Desktop\gaze_data.txt", FileMode.Append);
-            StreamWriter sw = new StreamWriter(fs);
+            // FileStream fs = new FileStream(@"C:\Users\nando\Desktop\gaze_data.txt", FileMode.Append);
+            // StreamWriter sw = new StreamWriter(fs);
 
-            // 3. Get the gaze data!
-            gazePointDataStream.GazePoint((x, y, ts) => {
-                string now = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
-                string point = string.Format(Format, (int)x, (int)y, now);
-                sw.WriteLine(point);
-                Console.WriteLine(point);
-            });
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
 
-            // okay, it is 4 lines, but you won't be able to see much without this one :)
-            Console.ReadKey();
+                // 3. Get the gaze data!
+                gazePointDataStream.GazePoint((x, y, ts) => {
+                    var y = x * 2.3;
+                    string now = DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss");
+                    string point = string.Format(Format, (int)x, (int)y, now);
+                    // sw.WriteLine(point);
+                    Console.WriteLine(point);
 
-            // we will close the coonection to the Tobii Engine before exit.
-            host.DisableConnection();
+                    using (var cmd = new NpgsqlCommand())
+                    {
+                        cmd.Connection = conn;
+                        cmd.CommandText = "INSERT INTO points (x, y, datetime) VALUES (@x, @y, @datetime)";
+                        cmd.Parameters.AddWithValue("x", "" + x);
+                        cmd.Parameters.AddWithValue("y", "" + y);
+                        cmd.Parameters.AddWithValue("datetime", DateTime.Now);
+                        cmd.ExecuteNonQuery();
+                    }
+                });
 
-            sw.Flush();
-            sw.Close();
-            fs.Close();
+                // okay, it is 4 lines, but you won't be able to see much without this one :)
+                Console.ReadKey();
+
+                // we will close the coonection to the Tobii Engine before exit.
+                host.DisableConnection();
+                conn.Close();
+                // sw.Flush();
+                // sw.Close();
+                // fs.Close();
+            }
         }
     }
 }
